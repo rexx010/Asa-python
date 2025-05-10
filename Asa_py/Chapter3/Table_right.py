be99c50b10a2b98dc56b486113d83836d71db979
rexx010 <grexx200@gmail.com>
2025-05-10 17:29:29 +0100

    Update Table_right.py

--- Asa_py/Chapter3/Table_right.py
+++ Asa_py/Chapter3/Table_right.py
@@ -3,4 +3,4 @@ print('number',	'square',	'cube')
 for number in range(6):
  digits = number * number
  cube = number ** 3
- print(number,'\t',digits,'\t',cube)
+ print(f'{number:>3}\t{digits:>3}\t{cube:>3}')
