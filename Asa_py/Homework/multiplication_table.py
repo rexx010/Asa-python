0ee33f034e3f2dd6eacbbde1818daa6d899c8e70
rexx010 <grexx200@gmail.com>
2025-05-11 20:44:09 +0100

    first  commit

---
+++ Asa_py/Homework/multiplication_table.py
@@ -0,0 +1,15 @@
+print('			multiplication_table')
+
+print('\t',1,'\t',2,'\t',3,'\t',4,'\t',5,'\t',6,'\t',7,'\t',8,'\t',9)
+print()
+for number in range(1, 10):
+ total = number * 1
+ total2 = number * 2
+ total3 = number * 3
+ total4 = number * 4
+ total5 = number * 5
+ total6 = number * 6
+ total7 = number * 7
+ total8 = number * 8
+ total9 = number * 9
+ print(number,'\t',total,'\t',total2,'\t',total3,'\t',total4,'\t',total5,'\t',total6,'\t',total7,'\t',total8,'\t',total9)
