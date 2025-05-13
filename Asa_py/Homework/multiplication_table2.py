ac877f2f4de912f604ea8f73b5a5a577740b3fb2
rexx010 <grexx200@gmail.com>
2025-05-13 19:50:05 +0100

    new multiplication table

--- Asa_py/Homework/multiplication_table2.py
+++ Asa_py/Homework/multiplication_table2.py
@@ -1,7 +1,9 @@
 print('\t','\t','Multiplication_table')
 for num in range(1, 10):
- print(f'{num:>3}', end='  ')
+ print(f'{num:>4}', end=' ')
+
 print() 
+print(f'-------------------------------------------------')
 for num in range(1, 10):
  print(f'{num:>2}|', end=' ')
 
