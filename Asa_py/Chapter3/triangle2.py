749acb5725b8239ad967aea222e43de59332c00e
rexx010 <grexx200@gmail.com>
2025-05-14 12:33:45 +0100

    changed the output

--- Asa_py/Chapter3/triangle2.py
+++ Asa_py/Chapter3/triangle2.py
@@ -1,5 +1,5 @@
-for c in range(1, 21):
-	for b in range(1, 21):
-		for a in range(1, 21):
+for a in range(1, 21):
+	for b in range(a, 21):
+		for c in range(b, 21):
 			if (c**2) == (a**2) + (b**2):
 					print(f"{c} = {a} + {b}")
