682b8478615036c1bdff7efaf38045c524e9484b
rexx010 <grexx200@gmail.com>
2025-05-14 12:32:31 +0100

    included the second highest

--- Asa_py/Chapter3/highest_number.py
+++ Asa_py/Chapter3/highest_number.py
@@ -1,9 +1,16 @@
 highest = 0
+highest2 = 0
 
-for number in range(0, 5):
+for number in range(1, 11):
   number = int(input('Enter a number: '))
 
   if number > highest:
+   highest2 = highest
    highest = number
 
+  elif number > highest2:
+   highest2 = number
+   
+
 print(f'The highest number is {highest}')
+print(f'The second highest number is {highest2}')
