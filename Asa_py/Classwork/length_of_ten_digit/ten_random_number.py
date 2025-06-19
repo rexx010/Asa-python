f2d26f3cdc5e3172c63a28dd6086799b7555015a
rexx010 <grexx200@gmail.com>
2025-06-19 20:27:40 +0100

    created a menu for ten random number

---
+++ Asa_py/Classwork/length_of_ten_digit/ten_random_number.py
@@ -0,0 +1,14 @@
+import ten_random_number_function
+
+nums = ten_random_number_function.creation_of_list()
+number_length = ten_random_number_function.the_length_of_the_list()
+summed = ten_random_number_function.the_sum_of_even_elements()
+summed_odd = ten_random_number_function.sum_of_odd_elements(nums)
+multiple = ten_random_number_function.elements_at_third_index()
+average = ten_random_number_function.calculate_all_element_average(number_length)
+print(nums)
+print(number_length)
+print(summed)
+print(summed_odd)
+print(multiple)
+print(average)
