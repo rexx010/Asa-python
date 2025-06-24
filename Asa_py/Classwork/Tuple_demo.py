0d114e6a9e7c76933789b336d63bd3b48bcbd43e
rexx010 <grexx200@gmail.com>
2025-06-24 16:47:43 +0100

    added isOdd function and filter functions to Tuple_demo.py

--- Asa_py/Classwork/Tuple_demo.py
+++ Asa_py/Classwork/Tuple_demo.py
@@ -21,10 +21,24 @@ def sorth_collection(collection):
     return collection
 print(sorth_collection(number))
 
+number_filter = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 def isOdd(number_filter):
     return number_filter % 2 == 1
 
-number_filter = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def filter_collection(collection):
+    return list(filter(isOdd, collection))
+print(filter_collection(number_filter))
+
 filtered_value = list(filter(isOdd, number_filter))
 print(filtered_value)
+
+def filter_with_lambda(collection):
+    return list(filter(lambda number: number % 2 == 1, collection))
+
+print(filter_with_lambda(number_filter))
+
+def sum_collection(number_filter):
+    sum_all_numbers = sum(map(lambda number: number, number_filter))
+    return sum_all_numbers
+print(sum_collection(number_filter))
