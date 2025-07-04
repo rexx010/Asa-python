6310911710a1cd10f9d0a89e5a55f3ddd30920c6
rexx010 <grexx200@gmail.com>
2025-07-05 00:50:51 +0100

    add a problem to the person's object created in python

---
+++ Asa_py/Classwork/problem.py
@@ -0,0 +1,11 @@
+class Problem:
+    def __init__(self, name, brand):
+        self.name = name
+        self.brand = brand
+        self.is_solved = False
+
+    def solve(self, name):
+        ...
+
+
+
