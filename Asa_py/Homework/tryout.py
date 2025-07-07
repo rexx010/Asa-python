575a17735463f0b6120e4ac7479ab8bdf91887ae
rexx010 <grexx200@gmail.com>
2025-07-07 09:16:33 +0100

    first commit

---
+++ Asa_py/Homework/tryout.py
@@ -0,0 +1,23 @@
+accounts = []
+def create_account(name, phone, bal=0.0):
+	account = [name, phone, bal]
+	accounts.append(account)
+	return account
+
+	
+def create_account_number(name, phone, bal=0.0):
+	account[0].insert(0, 'wande')
+	return account[0]
+
+print(create_account("ola", "08088406240","0.00"))
+	
+	
+
+
+
+
+'''
+name = input('Enter your name: ')
+number = int(input('Enter your number: '))
+
+print(create_account(name, number))'''
