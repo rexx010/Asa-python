787670f37b91467518ed9ed85c84633474238748
rexx010 <grexx200@gmail.com>
2025-07-07 09:14:43 +0100

    first commit

---
+++ Asa_py/Classwork/MultipleNoAst.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MultipleNoAst{
+public static void main(String[] args){
+
+Scanner input = new Scanner(System.in);
+
+System.out.print("Enter an integer value");
+int num1 = input.nextInt();
+System.out.print("Enter an integer value");
+int num2 = input.nextInt();
+
+for(int count = 1; count <= num2; count++){
+int result += num1;
+}
+System.out.print(result);
+
+}
+}
