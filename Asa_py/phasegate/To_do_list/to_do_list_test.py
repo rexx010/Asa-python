a9394bd2e5781cdf777b7bce749f10cfcbda3138
rexx010 <grexx200@gmail.com>
2025-07-19 11:57:52 +0100

    Update to_do_list_test.py

--- Asa_py/phasegate/To_do_list/to_do_list_test.py
+++ Asa_py/phasegate/To_do_list/to_do_list_test.py
@@ -7,9 +7,7 @@ class TestCube(TestCase):
 	def test_that_create_function_exits(self):
 		to_do_list_function.create_console('name')
 
-	def test_that_view_tasks(self):
-		to_do_list_function.view_tasks()
-		actual = to_do_list_function.view_tasks()
-		expected = ['tobi']
-		self.assertEqual(actual, expected)	
-		
+	def test_that_function_mark_task_completed(self):
+		actual = to_do_list_function.mark_task_completed('tobi[X]')
+		expected = 'tobi[X]'
+		self.assertEqual(actual, expected)
