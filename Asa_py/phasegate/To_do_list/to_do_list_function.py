7c3475bb6f06dc76d9e748c906a9be96b5d00702
rexx010 <grexx200@gmail.com>
2025-07-19 11:57:20 +0100

    Update to_do_list_function.py

--- Asa_py/phasegate/To_do_list/to_do_list_function.py
+++ Asa_py/phasegate/To_do_list/to_do_list_function.py
@@ -1,22 +1,20 @@
 todo_s = []
 
 def create_console(task):
-	todo = [task]
+	todo = task
 	todo_s.append(todo)
 
 def view_tasks():
-	for tasks in todo_s:
-		print(tasks)
+	for index, value in enumerate(todo_s):
+		print(index, value)
 
 def mark_task_completed(choose):
-	for item in todo_s:
-		if item == choose:
-			todo_s[item] = ('[X]')
+	for index, value in enumerate(todo_s):
+		if index == choose:
+			todo_s[choose] += '[X]'
 			
 
 def delete_task_completed(choose):
-	for task in todo_s:
-		if task == choose:
-			todo_s[0].remove
-
-
+	for index, value in enumerate(todo_s):
+		if index == choose:
+			todo_s.pop(choose)
