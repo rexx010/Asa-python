e7fa4059604d166276fede7e128329bfc520a2c5
rexx010 <grexx200@gmail.com>
2025-07-25 12:11:23 +0100

    created main for the car oop class

---
+++ Asa_py/OOP/car/Car_main.py
@@ -0,0 +1,10 @@
+from car import Car
+
+car1 = Car("BMW", 2024, "Black", False)
+car2 = Car("Toyota", 2024, "Red", True)
+car3 = Car("Honda", 2024, "Blue", True)
+
+
+car1.drive()
+car1.stop()
+car1.description()
