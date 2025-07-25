e9baa319f656d4d4444d86b5df46c18c031de2e9
rexx010 <grexx200@gmail.com>
2025-07-25 12:13:07 +0100

    created multiple inheritance class in oop py

---
+++ Asa_py/OOP/inheritance/multple_inheritance/multiple_inheritance.py
@@ -0,0 +1,33 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Prey(Animal):
+    def flee(self):
+        print(f"{self.name} is fleeing")
+
+class Predator(Animal):
+    def hunt(self):
+        print(f"{self.name} is hunting")
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit("Bunny")
+hawk = Hawk("Hawky")
+fish = Fish("Goldy")
+
+hawk.eat()
+
