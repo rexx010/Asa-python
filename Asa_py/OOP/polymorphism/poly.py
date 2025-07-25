6d110caab8d358b2a6e44cdd8fd8d0d498417c9d
rexx010 <grexx200@gmail.com>
2025-07-25 12:15:23 +0100

    worked around polymorphism in oop

---
+++ Asa_py/OOP/polymorphism/poly.py
@@ -0,0 +1,42 @@
+from Asa_py.OOP.super_class_function.shapes import circle, square
+from abc import ABC, abstractmethod
+
+
+class Shape:
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base * self.height * 0.5
+
+class Pizza(Circle):
+    def __init__(self, toppings, radius):
+        super().__init__(radius)
+        self.toppings = toppings
+        self.radius = radius
+
+shapes = (Circle(4), Square(5), Triangle(6, 7), Pizza("pepperoni", 15))
+
+for shape in shapes:
+    print(f"{shape.area()}cm²")
